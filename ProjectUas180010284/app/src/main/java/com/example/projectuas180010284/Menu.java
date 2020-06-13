d5a1c435d8e9cd405e2d852238fbfed2e484d61c
Kresna Dwipa Prayoga <64939648+yuraizuki@users.noreply.github.com>
2020-06-13 16:09:06 +0800

    upload program
    upload data yang sudah jadi

---
+++ ProjectUas180010284/app/src/main/java/com/example/projectuas180010284/Menu.java
@@ -0,0 +1,26 @@
+package com.example.projectuas180010284;
+
+public class Menu {
+
+    private String nama;
+    private String harga;
+    private String gambar;
+
+    public Menu(String datanama,String dataharga,String datagambar){
+        nama = datanama;
+        harga = dataharga;
+        gambar = datagambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+}
